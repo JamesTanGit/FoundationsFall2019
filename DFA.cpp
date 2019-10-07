3c979a15630e4d9cbe2931a811669983245a8ed2
JamesTanGit <34989692+JamesTanGit@users.noreply.github.com>
2019-10-07 07:23:06 -0400

    Completed Task #8
    Write a dozen example DFAs.

--- DFA.cpp
+++ DFA.cpp
@@ -81,21 +81,107 @@ int DFA<T>::state()
 int main(int argc, char* argv[])
 {
 	char input;
-	char input2;
-	cout << "Give me a character so I can return a DFA that accepts only strings of exactly that character: ";
-	cin >> input2;
-	cin.ignore(256, '\n');
-	DFA<char> dfa(0, false);
-	dfa.add_state(1, true);
-	dfa.add_transition(0, input2, 1);
-	dfa.add_transition(1, input2, 1);
+	DFA<char> dfa1(0, false);
+	// Checks if DFA is my name
+	dfa1.add_state(1, true);
+	dfa1.add_state(2, true);
+	dfa1.add_state(3, true);
+	dfa1.add_state(4, true);
+	dfa1.add_state(5, true);
+	dfa1.add_transition(0, 'j', 1);
+	dfa1.add_transition(1, 'a', 2);
+	dfa1.add_transition(2, 'm', 3);
+	dfa1.add_transition(3, 'e', 4);
+	dfa1.add_transition(4, 's', 5);
+	// Checks for even length string from alphabet 01
+	DFA<char> dfa2(0, true);
+	dfa2.add_state(1, false);
+	dfa2.add_transition(0, '0', 1);
+	dfa2.add_transition(0, '1', 1);
+	dfa2.add_transition(1, '0', 0);
+	dfa2.add_transition(1, '1', 0);
+	// Checks for strings that contain only 1's from alphabet 01
+	DFA<char> dfa3(0, false);
+	dfa3.add_state(1, true);
+	dfa3.add_transition(0, '1', 1);
+	dfa3.add_transition(1, '1', 1);
+	// Checks if the string is a binary number
+	DFA<char> dfa4(0, false);
+	dfa4.add_state(1, true);
+	dfa4.add_transition(0, '0', 1);
+	dfa4.add_transition(0, '1', 1);
+	dfa4.add_transition(1, '0', 1);
+	dfa4.add_transition(1, '1', 1);
+	// Checks if the DFA is an even number
+	DFA<char> dfa5(0, false);
+	dfa5.add_state(1, true);
+	dfa5.add_transition(0, '0', 1);
+	dfa5.add_transition(0, '1', 0);
+	dfa5.add_transition(1, '1', 0);
+	dfa5.add_transition(1, '0', 1);
+	// Checks if the DFA is an odd number
+	DFA<char> dfa6(0, false);
+	dfa6.add_state(1, true);
+	dfa6.add_transition(0, '1', 1);
+	dfa6.add_transition(0, '0', 0);
+	dfa6.add_transition(1, '0', 0);
+	dfa6.add_transition(1, '1', 1);
+	// Checks for strings that contain only 0's from alphabet 01
+	DFA<char> dfa7(0, false);
+	dfa7.add_state(1, true);
+	dfa7.add_transition(0, '0', 1);
+	dfa7.add_transition(1, '0', 1);
+	// Checks if a word contains the word cat inside of it
+	DFA<char> dfa8(0, false);
+	dfa8.add_state(1, false);
+	dfa8.add_state(2, false);
+	dfa8.add_state(3, true);
+	dfa8.add_transition(0, 'c', 1);
+	dfa8.add_transition(1, 'a', 2);
+	dfa8.add_transition(2, 't', 3);
+	dfa8.add_transition(-1, 'c', 1);
+	// Checks if a word does not contain the word cat inside of it
+	DFA<char> dfa9(0, true);
+	dfa9.add_state(1, true);
+	dfa9.add_state(2, true);
+	dfa9.add_state(3, false);
+	dfa9.add_transition(0, 'c', 1);
+	dfa9.add_transition(1, 'a', 2);
+	dfa9.add_transition(2, 't', 3);
+	dfa9.add_transition(-1, 'c', 1);
+	// Checks if the string is in the formet of /*/ with any number of *
+	DFA<char> dfa10(0, false);
+	dfa10.add_state(1, false);
+	dfa10.add_state(2, false);
+	dfa10.add_state(3, true);
+	dfa10.add_transition(0, '/', 1);
+	dfa10.add_transition(1, '/', 3);
+	dfa10.add_transition(1, '*', 2);
+	dfa10.add_transition(2, '*', 2);
+	dfa10.add_transition(2, '/', 3);
+	// Checks if the string is in the formet of /*/ with at least one *
+	DFA<char> dfa11(0, false);
+	dfa11.add_state(1, false);
+	dfa11.add_state(2, false);
+	dfa11.add_state(3, true);
+	dfa11.add_transition(0, '/', 1);
+	dfa11.add_transition(1, '*', 2);
+	dfa11.add_transition(2, '*', 2);
+	dfa11.add_transition(2, '/', 3);
+	// Checks to see if the number in the string is bigger than 0 in binary
+	DFA<char> dfa12(0, false);
+	dfa12.add_state(1, true);
+	dfa12.add_transition(0, '0', 0);
+	dfa12.add_transition(0, '1', 1);
+	dfa12.add_transition(1, '0', 1);
+	dfa12.add_transition(1, '1', 1);
 	while (true)
 	{
-		cout << "State: " << dfa.state() << "  " << (dfa.is_accepting() ? "true" : "false") << endl;
+		cout << "State: " << dfa1.state() << "  " << (dfa1.is_accepting() ? "true" : "false") << endl;
 		if (cin.peek() == '\n')
 			break;
 		cin >> noskipws >> input;
-		dfa.input(input);
+		dfa1.input(input);
 	}
-  return 0;
+  	return 0;
 }
